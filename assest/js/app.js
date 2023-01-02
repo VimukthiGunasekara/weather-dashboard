459582c68edaf37772cbea3c2fd0ffc59f0fac90
Vimukthi Gunasekara <vimukthisadaruwan65@gmail.com>
2023-01-02 11:02:00 -0500

    get 5Days weather forecasting

--- assest/js/app.js
+++ assest/js/app.js
@@ -14,8 +14,6 @@ var submitBtn = document.querySelector('#submitBtn');
 var apiKey = "0060eaa04b33f5f3c1022821bc49909c";
 
 var getCurrentWeather = () => {
-
-
     var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=" + apiKey;
     fetch(url)
         .then(function (respose) {
@@ -34,12 +32,99 @@ var getCurrentWeather = () => {
         });
 }
 
+var getFiveDaysForeCasting = () => {
+
+    var fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&units=imperial&APPID=" + apiKey;
+
+    fetch(fiveDay)
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (data) {
+
+            let fiveDayForecastHTML = `<div">
+
+            <hr><h4 class="text-center">05 more days weather situations</h4>`;
+
+            for (let i = 0; i < data.list.length; i++) {
+                let dateofday = data.list[i];
+                let dayTime = dateofday.dt;
+                let timeZone = data.city.timezone;
+                let timeZoneHours = timeZone / 60 / 60;
+                let thisMoment = moment.unix(dayTime).utc().utcOffset(timeZoneHours);
+                let iconURL = "https://openweathermap.org/img/w/" + dateofday.weather[0].icon + ".png";
+
+                if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
+
+                    fiveDayForecastHTML += `<div class="medium-3 columns">
+                    <div class="card fiveday-card">
+                        <div class="card-divider">
+                            <h5>${thisMoment.format("DD MMM YYYY")}</h5>
+                        </div>
+                        <img src="${iconURL}">
+                        <div class="card-section">
+                            <h4>Temp: ${dateofday.main.temp}&#8457;</h4>
+                            <p>Feel like: ${dateofday.main.feels_like}%</p>
+                            <p>Humidity: ${dateofday.main.humidity}%</p>
+                        </div>
+                    </div>
+                </div>`;
+                }
+            }
+            fiveDayForecastHTML += `</div>`;
+            $('#five-day-forecast').html(fiveDayForecastHTML);
+        });
+};
+
+
+var getFiveDaysForeCasting = () => {
+
+    var fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&units=imperial&APPID=" + apiKey;
+
+    fetch(fiveDay)
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (data) {
+
+            let fiveDayForecastHTML = `<div">
+
+            <hr><h4 class="text-center">05 more days weather situations</h4>`;
+
+            for (let i = 0; i < data.list.length; i++) {
+                let dateofday = data.list[i];
+                let dayTime = dateofday.dt;
+                let timeZone = data.city.timezone;
+                let timeZoneHours = timeZone / 60 / 60;
+                let thisMoment = moment.unix(dayTime).utc().utcOffset(timeZoneHours);
+                let iconURL = "https://openweathermap.org/img/w/" + dateofday.weather[0].icon + ".png";
+
+                if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
+
+                    fiveDayForecastHTML += `<div class="medium-3 columns">
+                    <div class="card fiveday-card">
+                        <div class="card-divider">
+                            <h5>${thisMoment.format("DD MMM YYYY")}</h5>
+                        </div>
+                        <img src="${iconURL}">
+                        <div class="card-section">
+                            <h4>Temp: ${dateofday.main.temp}&#8457;</h4>
+                            <p>Feel like: ${dateofday.main.feels_like}%</p>
+                            <p>Humidity: ${dateofday.main.humidity}%</p>
+                        </div>
+                    </div>
+                </div>`;
+                }
+            }
+            fiveDayForecastHTML += `</div>`;
+            $('#five-day-forecast').html(fiveDayForecastHTML);
+        });
+};
+
 function getWForeCasting() {
     getCurrentWeather();
+    getFiveDaysForeCasting();
 }
 
 getWForeCasting();
 submitBtn.addEventListener("click", getWForeCasting);
-
-
-
